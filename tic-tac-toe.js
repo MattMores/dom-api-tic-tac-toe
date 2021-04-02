faaf6f5c620b5a9c176272a86815403670e67eb3
Joe <Joe.zhang229@gmail.com>
2021-04-02 14:22:53 -0700

    phase 1 complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,19 +1,30 @@
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
+	let currentPlayerSymbol = 'x';
 
- let currentPlayerSympol = "x"
+	let squareValues = ['', '', '', '', '', '', '', '', ''];
 
- let squareValues = ["", "", "",
-                    "", "", "",
-                    "", "", ""]
+	let gameBoard = document.querySelector('#tic-tac-toe-board');
 
- let gameBoard = document.querySelector("#tic-tac-toe-board")
-
- gameBoard.addEventListener("click", (event) => {
-    let [firstPart, id] = event.target.id.split("-")
-    let boardNumber = Number(id)
-    if (squareValues[boardNumber] !== "") {
-        return
-    }
- })
-
+	gameBoard.addEventListener('click', (event) => {
+		let [firstPart, id] = event.target.id.split('-');
+		let boardNumber = Number(id);
+        // if board has something, ignore everything below
+		if (squareValues[boardNumber] !== '') {
+			return;
+		}
+		// logic later
+		let img = document.createElement('img');
+		if (currentPlayerSymbol === 'x') {
+			img.src =
+				'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg';
+			currentPlayerSymbol = 'o';
+			event.target.appendChild(img);
+		} else if (currentPlayerSymbol === 'o') {
+			img.src =
+				'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg';
+			currentPlayerSymbol = 'x';
+			event.target.appendChild(img);
+		}
+		// event.target.appendChild(img);
+	});
 });
